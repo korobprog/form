e5d91818bf57297847137a7543e6d633956022f9
korobprog <korobprog@gmail.com>
2023-07-10 17:19:25 +1000

    finish3

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import "./css/style.css";
-import "./js/app";
-import goblin from "./img/goblin.png";
+import './css/style.css';
+import './js/app';
+import goblin from './img/goblin.png';
 
-const cells = document.querySelectorAll(".cell");
-const character = document.createElement("img");
+const cells = document.querySelectorAll('.cell');
+const character = document.createElement('img');
 
 character.src = goblin;
 
 function spawnCharacter() {
-   const randomCell = cells[Math.floor(Math.random() * cells.length)];
-   randomCell.appendChild(character);
+  const randomCell = cells[Math.floor(Math.random() * cells.length)];
+  randomCell.appendChild(character);
 }
 
 function moveCharacter() {
-   const currentCell = character.parentElement;
-   const possibleCells = [...cells].filter((cell) => cell !== currentCell);
-   const randomCell =
-      possibleCells[Math.floor(Math.random() * possibleCells.length)];
-   randomCell.appendChild(character);
+  const currentCell = character.parentElement;
+  const possibleCells = [...cells].filter((cell) => cell !== currentCell);
+  const randomCell =
+    possibleCells[Math.floor(Math.random() * possibleCells.length)];
+  randomCell.appendChild(character);
 }
 
 spawnCharacter();
