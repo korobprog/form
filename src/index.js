ff2069f4a6c3d75a677743f1e1da2c6dbfe15370
korobprog <korobprog@gmail.com>
2023-09-18 18:47:15 +1000

    inicial commit styles

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import './css/style.css';
 import './js/app';
-import goblin from './img/goblin.png';
 
-const cells = document.querySelectorAll('.cell');
-const character = document.createElement('img');
+const btn = document.getElementById('btn');
+const popover = document.getElementById('popover');
 
-character.src = goblin;
-
-function spawnCharacter() {
-  const randomCell = cells[Math.floor(Math.random() * cells.length)];
-  randomCell.appendChild(character);
-}
-
-function moveCharacter() {
-  const currentCell = character.parentElement;
-  const possibleCells = [...cells].filter((cell) => cell !== currentCell);
-  const randomCell =
-    possibleCells[Math.floor(Math.random() * possibleCells.length)];
-  randomCell.appendChild(character);
-}
-
-spawnCharacter();
-setInterval(moveCharacter, 1000);
+btn.addEventListener('click', () => {
+  popover.classList.toggle('hidden');
+});
